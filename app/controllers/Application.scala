f3c351223d03fb9faf13cd9111533e7d44e9e87a
Administrator <arkadiusk.kowalski.qa.com>
2017-07-26 09:40:58 +0100

    first push1 dev

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,10 +5,110 @@ import play.api.mvc._
 
 class Application extends Controller {
 
-  def index = Action {
-    Ok("Hello world") // import
-    //Ok(views.html.index("Your new application is ready."))
+  val echoReq = Action {implicit request: Request[AnyContent]=>
+    Ok("Got Request ["+request+ "]")
   }
+
+  val index = Action{
+    Ok("static echo")
+  }
+
+  def secondPage = Action{
+    Ok(views.html.secondPage("you have been directed to the second page"))
+  }
+
+  def indexWithInput(input:String) = Action{
+    Ok(views.html.index(input))
+  }
+  def doProcessing(input:String) = Action{
+    Ok(views.html.secondPage("I see you have added: " + input))
+  }
+
+  import play.api.http.HttpEntity
+  import akka.util.ByteString
+  def customResult = Action {
+    Result(
+      header = ResponseHeader(200, Map.empty),
+      body = HttpEntity.Strict(ByteString("Hello world! Custom Result"), Some("text/plain"))
+    )
+  }
+
+  val simpleRedirect = Action{
+    Redirect("/secondPage", MOVED_PERMANENTLY)
+  }
+
+  val testThis = Action {
+    Redirect(routes.Application.plannedUpdates())
+  }
+
+  def plannedUpdates = Action {
+    Ok("Planned technical updates are in progress.")
+  }
+
+  val ok =Ok("Hello world!")
+  val notFound = NotFound
+  val pageNotFound = NotFound(<h1>Page not found</h1>)
+  val badRequest = BadRequest("Error happened")
+  val oops = InternalServerError("Oops")
+  val anyStatus = Status(488)("Strange response type")
+
+  val helper1 = Action {
+    anyStatus
+  }
+
+  def takeMeToTheCandyShop = TODO
+
+
+  def startCookie = Action{
+    Ok("it works!").withCookies(
+      Cookie("theme","blue")
+    )
+  }
+
+  def discardCookie = Action{implicit request:Request[AnyContent]=>
+    Ok("Discarding: " + request.cookies.get("theme").get.value).discardingCookies(
+      DiscardingCookie("theme"))
+  }
+  def changeCookieValue = Action{implicit request:Request[AnyContent]=>
+    Ok("Was this theme: " + request.cookies.get("theme").get.value).withCookies(
+      Cookie("theme","green")
+    )
+  }
+
+  def startSession = Action{
+    Ok("hello").withSession(
+      "connected" -> "emailAddress")
+
+  }
+  def addSessionElement = Action{implicit request: Request[AnyContent]=>
+    Ok("Hello World!").withSession(
+      request.session + ("saidHello" -> "yes"))
+  }
+  def getSessionElement = Action{implicit request: Request[AnyContent] =>
+    Ok("sessionElement: " + request.session.get("saidHello").getOrElse("dunno"))
+  }
+
+  def cutSession = Action{implicit request: Request[AnyContent] =>
+    Ok("end of session").withNewSession
+  }
+
+
+
+  def flashRedirect = Action { implicit request =>
+    Ok {
+      request.flash.get("nameOfFlashVariable").getOrElse("AlternativeToFlashContent!")
+    }
+  }
+
+  def flash = Action {
+    Redirect("/flashRedirect").flashing(
+      "nameOfFlashVariable" -> "The item has been created")
+  }
+
+
+
+
+
 
 }
 
---
+++ app/controllers/CDApplication.scala
@@ -0,0 +1,39 @@
+package controllers
+
+import javax.inject.Inject
+
+import models.CD
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc._
+
+import scala.concurrent.Future
+
+class CDApplication @Inject()(val messagesApi: MessagesApi) extends Controller with I18nSupport {
+
+  def index = Action {
+    Ok(views.html.index("Your new application is ready."))
+  }
+
+  def listCDs = Action { implicit request =>
+    Ok(views.html.listCDs(CD.cds, CD.createCDForm))
+  }
+
+  def createCD = Action { implicit request =>
+
+    val formValidationResult = CD.createCDForm.bindFromRequest
+    formValidationResult.fold({ formWithErrors =>
+      BadRequest(views.html.listCDs(CD.cds, formWithErrors))
+    }, { cd =>
+      CD.cds.append(cd)
+      Redirect(routes.CDApplication.listCDs)
+    })
+  }
+
+
+
+
+
+
+}
+
+
